7744d6918de40649212c45a85a13364de48701b1
Yaser Almasri <yaser@masys.mx>
2020-12-11 16:36:43 -0600

    rescue from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError do |err|
+    render(
+      json: {
+        errors: err.message,
+        messages: err.message,
+        details: err.message
+      },
+      status: 422
+    )
+  end
 end
